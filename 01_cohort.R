58c2500719d76f2cfe107f2e4869a481a07472fe
plyons <patrick.g.lyons@proton.me>
2025-08-20 09:37:43 -0700

    Update 01_cohort.R
    add characteristics/outcomes table to the end of this

--- 01_cohort.R
+++ 01_cohort.R
@@ -865,6 +865,103 @@ if (
 write_parquet(cohort,            here("proj_tables", "cohort.parquet"))
 write_parquet(hid_jid_crosswalk, here("proj_tables", "hid_jid_crosswalk.parquet"))
 
+# t02. characteristics/outcomes by cancer status (0 = none, 1 = cancer) --------
+
+## prepare table component = continuous variables ------------------------------
+
+t2_cont = 
+  fgroup_by(cohort, ca_01) |>
+  fsummarize(
+    n         = fnobs(joined_hosp_id),
+    age_sum   = fsum(age,          na.rm = TRUE),
+    age_sumsq = fsum(age^2,        na.rm = TRUE),
+    age_p025  = fquantile(age, 0.025),
+    age_p975  = fquantile(age, 0.975),
+    los_sum   = fsum(los_hosp_d,   na.rm = TRUE),
+    los_sumsq = fsum(los_hosp_d^2, na.rm = TRUE),
+    los_p025  = fquantile(los_hosp_d, 0.025),
+    los_p975  = fquantile(los_hosp_d, 0.975)
+  ) |>
+  ftransform(var_type = "continuous") |>
+  ftransform(site     = paste0(site_lowercase))
+
+## prepare table component = categorized continuous variables ------------------
+
+### age ------------------------------------------------------------------------
+
+age_breaks = c( 18,      40,      50,      60,      70,      80,  Inf)
+age_labs   = c("18_39", "40_49", "50_59", "60_69", "70_79", "80_plus")
+
+ages_cat = 
+  select(cohort, ca_01, age) |>
+  fmutate(a = cut(age, breaks = age_breaks, labels = age_labs, right = F)) |>
+  fgroup_by(ca_01, a) |>
+  fnobs() |>
+  select(ca_01, age_cat = a, n = age) |>
+  ftransform(age_cat = paste0("age_", age_cat)) |>
+  ftransform(var = "age", category = str_remove(age_cat, "age_")) |>
+  select(ca_01, var, category, n) 
+
+### los (days) -----------------------------------------------------------------
+
+l_breaks = c( 0,        2,         4,         7,         14, Inf)
+l_labs   = c("0-47h", "48h_96h", "96h_1wk", "1wk_2wk", "2wk_plus")
+
+los_cat = 
+  select(cohort, ca_01, los_hosp_d) |>
+  fmutate(los_cat = cut(los_hosp_d, breaks = l_breaks, labels = l_labs, right = F)) |>
+  fgroup_by(ca_01, los_cat) |>
+  fnobs() |>
+  select(ca_01, los_cat, n = los_hosp_d) |>
+  ftransform(var = "los", category = los_cat) |>
+  select(ca_01, var, category, n) 
+
+## prepare table component = categorical variables -----------------------------
+
+t2_cat = 
+  select(cohort, -ends_with("id"), -ends_with("dttm"),  -age, -los_hosp_d) |>
+  pivot_longer(-ca_01, names_to = "var", values_to = "val") |>
+  fsubset(!is.na(val)) |>
+  fmutate(n = val) |>
+  fgroup_by(ca_01, var, val) |>
+  fnobs() |>
+  ftransform(var      = str_remove(var, "_01")) |>
+  ftransform(category = tolower(str_replace_all(as.character(val), "-", "_"))) |>
+  select(ca_01, var, category, n) 
+
+## quality control -------------------------------------------------------------
+
+### check for small n in cells -------------------------------------------------
+
+ages_cat$site = site_lowercase
+los_cat$site  = site_lowercase
+t2_cat$site   = site_lowercase
+
+all_cat = rowbind(ages_cat, los_cat, t2_cat)
+small_c = fsubset(all_cat, n < 5 & n > 0)
+
+if (nrow(small_c) > 0) {
+  msg = paste0(
+    "ERROR: ", nrow(small_c), " cells have n < 5:\n",
+    capture.output(print(small_c |> fselect(var, category, ca_01, n))) |> 
+      paste(collapse = "\n")
+  )
+  stop(msg)
+}
+
+### check for sample size mismatch in continuous table -------------------------
+
+if (sum(t2_cont$n) != nrow(cohort)) {
+  stop("ERROR: Sample size mismatch! Sum of t2_cont$n != nrow(df)")
+}
+
+## export table 2 --------------------------------------------------------------
+
+fwrite(all_cat, here("proj_output", paste0("table_02_cat_",  site_lowercase, ".csv")))
+fwrite(t2_cont, here("proj_output", paste0("table_02_cont_", site_lowercase, ".csv")))
+
+# final cleanup ----------------------------------------------------------------
+
 keep = c(
   "data_list",
   "site_lowercase",
