75206aa951f5bdf929bc8b623652fc5891b083ae
plyons <patrick.g.lyons@proton.me>
2025-11-14 09:30:38 -0800

    Update source paths in run_all.R

--- run_all.R
+++ run_all.R
@@ -2,15 +2,15 @@
 # run_all.R
 
 message("Starting data import/validation...")
-source("code/00_setup.R")
+source("00_setup.R")
 
 message("Cleaning data...")
-source("code/01_cohort.R")
+source("01_cohort.R")
 
 message("Calculating scores...")
-source("code/02_scores.R")
+source("02_scores.R")
 
 message("Performing analysis...")
-source("code/03_analysis.R")
+source("03_analysis.R")
 
 message("Pipeline completed")
