67279fda545b1f19b11026c0c022b09004be8582
plyons <patrick.g.lyons@proton.me>
2026-01-22 14:58:08 -0800

    Update rowbind to include los_cat

--- 01_cohort.R
+++ 01_cohort.R
@@ -1103,7 +1103,8 @@ t2_cat =
   ftransform(var      = str_remove(var, "_01")) |>
   ftransform(category = tolower(str_replace_all(as.character(val), "-", "_"))) |>
   fselect(ca_01, var, category, n) |>
-  rowbind(elix_cat)
+  rowbind(elix_cat) |>
+  rowbind(los_cat)
 
 ## quality control -------------------------------------------------------------
 
